089dc70de67461c389efe890877b12c61613557f
Kirill Arefev <arefieff@mail.ru>
2015-09-30 03:20:03 +0800

    html test

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -20,57 +20,54 @@ AppAsset::register($this);
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu" />
+    <link rel="stylesheet" type="text/css" href="/css/site.css" />
 </head>
 <body>
-    <?php $this->beginBody() ?>
-    <div class="wrap">
-        <?php
-            NavBar::begin([
-                'brandLabel' => 'My Company',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            $menuItems = [
-                ['label' => 'Home', 'url' => ['/site/index']],
-                ['label' => 'About', 'url' => ['/site/about']],
-                ['label' => 'Contact', 'url' => ['/site/contact']],
-            ];
-            if (Yii::$app->user->isGuest) {
-                $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-                $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-            } else {
-                $menuItems[] = [
-                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ];
-            }
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => $menuItems,
-            ]);
-            NavBar::end();
-        ?>
-
-        <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+    <header>
+        <div class="grid">
+            <div class="row">
+                <div class="col11">
+                    <a class="logo" href="/">
+                        <img src="/i/logo.png" alt="Blog"/>
+                    </a>
+                </div>
+                <div class="col1 rss">RSS</div>
+            </div>
+        </div>
+    </header>
+    <div class="content">
+        <h1 class="blog-name">Blog</h1>
+        <div class="row">
+            <div class="post-list col9">
+                <div class="post row">
+                    <div class="post-date col12">July 5, 2015</div>
+                    <div class="post-name col12">
+                        <h2>Cómo aumentar tu tráfico web de 4.000 a 40.000 visitas en solo 11 meses</h2>
+                        <div class="post-author">by Yulia Shevardenkova</div>
+                    </div>
+                    <div class="post-img col4">
+                        <img src="/i/test-post-img.jpg" alt="Post #1"/>
+                    </div>
+                    <div class="post-short-text col8">
+                        If you run a small or new website, one thing you will always struggle with is the crawl rate within Google. If you add your 10 page website to Google with a few targeted keywords within the title tags and some cleverly placed variations within the content, you’re going to rank well, right? Well, that’s not always the case.
+                    </div>
+                </div>
+            </div>
+            <div class="ad col3">
+                <img src="/i/banner.jpg" alt="Banner"/>
+            </div>
         </div>
     </div>
-
-    <footer class="footer">
-        <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="pull-right"><?= Yii::powered() ?></p>
+    <footer>
+        <div class="grid">
+            <div class="row">
+                <div class="col3">
+                    <img class="logo" src="/i/logo.png" alt="Blog"/>
+                </div>
+                <div class="col9 copy">&copy; 2008–2015 SEMrush. All rights reserved.</div>
+            </div>
         </div>
     </footer>
-
-    <?php $this->endBody() ?>
 </body>
 </html>
-<?php $this->endPage() ?>
